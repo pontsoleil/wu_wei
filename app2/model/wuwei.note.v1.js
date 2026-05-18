8e0fb061d22a89a7b087275a371ea14dec90bb1d
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2026-05-18 16:34:47 +0900

    Update wuwei.note.v1.js

--- app2/model/wuwei.note.v1.js
+++ app2/model/wuwei.note.v1.js
@@ -194,7 +194,7 @@ wuwei.note.v1 = (function () {
     var suffixIndex;
     var match;
 
-    if (!raw || /^(data:|blob:|mailto:|tel:)/i.test(raw)) {
+    if (!raw || /^(https?:\/\/|data:|blob:|mailto:|tel:)/i.test(raw)) {
       return raw;
     }
 
